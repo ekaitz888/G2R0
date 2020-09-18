64d32e699ea66738032b0aabef5769ca36286fbb
ik_2dm3 <ik_2dm3@H015110.NET>
2020-09-18 12:06:17 +0200

    activado y cambios en el id de la sala

--- BBDD/g2r0.sql
+++ BBDD/g2r0.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-09-2020 a las 11:21:20
+-- Tiempo de generación: 18-09-2020 a las 12:05:11
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.4.10
 
@@ -27,11 +27,11 @@ DELIMITER $$
 --
 -- Procedimientos
 --
-CREATE DEFINER=`root`@`localhost` PROCEDURE `AlarmaMod` (IN `pID_Salas` INT(3))  NO SQL
-insert INTO alarma (ID_Sala, fechaMod) VALUES (pID_Sala, NOW())$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `AlarmaMod` (IN `pID_Salas` VARCHAR(7), IN `pActivado` VARCHAR(40))  NO SQL
+insert INTO alarma (ID_Sala, fechaMod, activado) VALUES (pID_Sala, NOW(),pActivado)$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `calefacciónMod` (IN `pID_Salas` INT(3))  NO SQL
-INSERT INTO calefaccion (ID_Sala, fechaMod) VALUES (pID_Sala, NOW())$$
+CREATE DEFINER=`root`@`localhost` PROCEDURE `calefacciónMod` (IN `pID_Salas` VARCHAR(7), IN `pActivado` VARCHAR(40))  NO SQL
+INSERT INTO calefaccion (ID_Sala, fechaMod, activado) VALUES (pID_Sala, NOW(), pActivado)$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `SelectAll` ()  NO SQL
 SELECT * FROM salas INNER JOIN calefaccion ON salas.ID_Sala=calefaccion.ID_Sala INNER JOIN alarma ON salas.ID_Sala=alarma.ID_Sala$$
@@ -46,8 +46,9 @@ DELIMITER ;
 
 CREATE TABLE `alarma` (
   `ID_Alarma` int(11) NOT NULL,
-  `ID_Sala` int(3) UNSIGNED NOT NULL,
-  `fechaMod` date NOT NULL
+  `ID_Sala` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `fechaMod` date NOT NULL,
+  `activado` varchar(40) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -58,8 +59,9 @@ CREATE TABLE `alarma` (
 
 CREATE TABLE `calefaccion` (
   `ID_Calefaccion` int(11) NOT NULL,
-  `ID_Sala` int(3) UNSIGNED NOT NULL,
-  `fechaMod` date NOT NULL
+  `ID_Sala` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `fechaMod` date NOT NULL,
+  `activado` varchar(40) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -69,7 +71,7 @@ CREATE TABLE `calefaccion` (
 --
 
 CREATE TABLE `salas` (
-  `ID_Sala` int(3) UNSIGNED NOT NULL,
+  `ID_Sala` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
   `SalaNum` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
   `Piso` enum('1','2','3') COLLATE utf8_unicode_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci COMMENT='Salas en el centro';
@@ -79,42 +81,42 @@ CREATE TABLE `salas` (
 --
 
 INSERT INTO `salas` (`ID_Sala`, `SalaNum`, `Piso`) VALUES
-(1, 'sala 001', '1'),
-(2, 'sala 002', '1'),
-(3, 'sala 003', '1'),
-(4, 'sala 004', '1'),
-(5, 'sala 005', '1'),
-(6, 'sala 006', '1'),
-(7, 'sala 007', '1'),
-(8, 'sala 008', '1'),
-(9, 'sala 009', '1'),
-(10, 'sala 010', '1'),
-(11, 'sala 011', '1'),
-(12, 'sala 012', '1'),
-(13, 'sala 101', '2'),
-(14, 'sala 102', '2'),
-(15, 'sala 103', '2'),
-(16, 'sala 104', '2'),
-(17, 'sala 105', '2'),
-(18, 'sala 106', '2'),
-(19, 'sala 107', '2'),
-(20, 'sala 108', '2'),
-(21, 'sala 109', '2'),
-(22, 'sala 110', '2'),
-(23, 'sala 111', '2'),
-(24, 'sala 112', '2'),
-(25, 'sala 201', '3'),
-(26, 'sala 202', '3'),
-(27, 'sala 203', '3'),
-(28, 'sala 204', '3'),
-(29, 'sala 205', '3'),
-(30, 'sala 206', '3'),
-(31, 'sala 207', '3'),
-(32, 'sala 208', '3'),
-(33, 'sala 209', '3'),
-(34, 'sala 210', '3'),
-(35, 'sala 211', '3'),
-(44, 'sala 212', '3');
+('P1_1', 'sala 001', '1'),
+('P1_10', 'sala 010', '1'),
+('P1_11', 'sala 011', '1'),
+('P1_12', 'sala 012', '1'),
+('P1_2', 'sala 002', '1'),
+('P1_3', 'sala 003', '1'),
+('P1_4', 'sala 004', '1'),
+('P1_5', 'sala 005', '1'),
+('P1_6', 'sala 006', '1'),
+('P1_7', 'sala 007', '1'),
+('P1_8', 'sala 008', '1'),
+('P1_9', 'sala 009', '1'),
+('P2_1', 'sala 101', '2'),
+('P2_10', 'sala 110', '2'),
+('P2_11', 'sala 111', '2'),
+('P2_12', 'sala 112', '2'),
+('P2_2', 'sala 102', '2'),
+('P2_3', 'sala 103', '2'),
+('P2_4', 'sala 104', '2'),
+('P2_5', 'sala 105', '2'),
+('P2_6', 'sala 106', '2'),
+('P2_7', 'sala 107', '2'),
+('P2_8', 'sala 108', '2'),
+('P2_9', 'sala 109', '2'),
+('P3_1', 'sala 201', '3'),
+('P3_10', 'sala 210', '3'),
+('P3_11', 'sala 211', '3'),
+('P3_12', 'sala 212', '3'),
+('P3_2', 'sala 202', '3'),
+('P3_3', 'sala 203', '3'),
+('P3_4', 'sala 204', '3'),
+('P3_5', 'sala 205', '3'),
+('P3_6', 'sala 206', '3'),
+('P3_7', 'sala 207', '3'),
+('P3_8', 'sala 208', '3'),
+('P3_9', 'sala 209', '3');
 
 --
 -- Índices para tablas volcadas
@@ -156,12 +158,6 @@ ALTER TABLE `alarma`
 --
 ALTER TABLE `calefaccion`
   MODIFY `ID_Calefaccion` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de la tabla `salas`
---
-ALTER TABLE `salas`
-  MODIFY `ID_Sala` int(3) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=45;
 
 --
 -- Restricciones para tablas volcadas
